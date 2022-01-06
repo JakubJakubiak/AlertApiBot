28c4c936947c2b176caec9c68d18e1aa963cbe30
Jakub Jakubiak <jebacyuu@gmail.com>
2022-01-06 02:43:59 +0100

    Api
    Api add

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,3 @@
-
-module.exports={
-token:'',
+module.exports = {
+  token: "",
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,67 @@
-const Discord = require("discord.js")
+const { Client, Intents} = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 const chalk = require("chalk")
 
 const config = require("./config/config.js")
-
-const client = new Discord.Client()
-
+const fetch = require('node-fetch')
 const log = console.log
 
 client.on("ready", () => {
   log(chalk.green(`login ${client.user.tag}!`))
 })
 
+
+let data = []
+
+
+const datas = () => {
+  const wallet = [
+  
+  ]
+
+  for(let i=0;i<wallet.length;i++){
+   fetch(`https://api.ethermine.org/miner/:${wallet[i]}/workers`,{
+     method: 'GET'
+   })
+  .then(res => res.json())
+  .then(res => { 
+    list = res.data
+      .filter(item => item.reportedHashrate*0.8 >= item.currentHashrate )
+      .map(item => `${item.worker} ${item.lastSeen} ${item.reportedHashrate} ${item.currentHashrate}`)
+      .join('\n')
+    
+    data += list
+
+  }) 
+  .catch((err) => console.log(err, "failed"))
+  }
+}
+datas()
+
+
+
+  
+
+
+
 client.on("message", (msg) => {
   const { author } = msg
+ 
 
-  // Check if user is a bot
-  if (author.bot) {
-    return
-  }
-
+  if (author.bot) return
   if (msg.content === "!ping") {
-    msg.reply("Pong!")
-  }
+    msg.reply("pong");
+   
+    setInterval(() => {
+    if(data.length > 0){
+      datas();
+      msg.reply(data)
+      data = "";
+    }
+  
+    },3000)
+}
 })
+
 
 client.login(config.token)
