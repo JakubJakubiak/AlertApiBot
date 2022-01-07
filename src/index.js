d991c847002f39ae50f723dd7f83d483168faede
Jakub Jakubiak <jebacyuu@gmail.com>
2022-01-08 00:19:47 +0100

    output power

--- src/index.js
+++ src/index.js
@@ -4,64 +4,58 @@ const chalk = require("chalk")
 
 const config = require("./config/config.js")
 const fetch = require('node-fetch')
+const match = require('nodemon/lib/monitor/match')
 const log = console.log
 
 client.on("ready", () => {
   log(chalk.green(`login ${client.user.tag}!`))
 })
 
-
 let data = []
 
-
 const datas = () => {
-  const wallet = [
+const wallet = []
   
-  ]
-
   for(let i=0;i<wallet.length;i++){
    fetch(`https://api.ethermine.org/miner/:${wallet[i]}/workers`,{
      method: 'GET'
    })
   .then(res => res.json())
   .then(res => { 
+    if(res.status === "OK"){
     list = res.data
-      .filter(item => item.reportedHashrate*0.8 >= item.currentHashrate )
-      .map(item => `${item.worker} ${item.lastSeen} ${item.reportedHashrate} ${item.currentHashrate}`)
-      .join('\n')
-    
+      .filter(item => item.currentHashrate*100/item.reportedHashrate <= 90)
+      .map(item => `
+        worker: ${item.worker}
+        lastSeen: ${item.lastSeen}
+        reportedHashrate: ${item.reportedHashrate}
+        currentHashrate: ${item.currentHashrate}
+        ${Math.floor(item.currentHashrate*100/item.reportedHashrate)} %
+        `)
+      .join('\n') 
     data += list
 
-  }) 
+
+  }}) 
   .catch((err) => console.log(err, "failed"))
   }
 }
 datas()
 
-
-
-  
-
-
-
 client.on("message", (msg) => {
   const { author } = msg
- 
 
+  // Check if user is a bot
   if (author.bot) return
-  if (msg.content === "!ping") {
-    msg.reply("pong");
+  if (msg.content === "!moris") {
+    msg.reply("Start");
    
     setInterval(() => {
     if(data.length > 0){
       datas();
       msg.reply(data)
       data = "";
-    }
-  
-    },3000)
-}
-})
-
+    }},3000)
+}})
 
 client.login(config.token)
