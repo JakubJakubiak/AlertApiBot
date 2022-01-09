e1b3f4d0bb0a03c88bc08da6e996ddc3d19a242e
Jakub Jakubiak <jebacyuu@gmail.com>
2022-01-09 23:12:30 +0100

    clock
    clock

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const { Client, Intents} = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const {
+	Client,
+	Intents
+} = require('discord.js')
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS]
+})
 const chalk = require("chalk")
 
 const config = require("./config/config.js")
@@ -8,54 +13,63 @@ const match = require('nodemon/lib/monitor/match')
 const log = console.log
 
 client.on("ready", () => {
-  log(chalk.green(`login ${client.user.tag}!`))
+	log(chalk.green(`login ${client.user.tag}!`))
 })
 
+let nTime = () => new Date().getTime() - 1000 * 60 * 60 * 24 * 7
+let oldTime = nTime()
 let data = []
+let err = true
 
 const datas = () => {
-const wallet = []
-  
-  for(let i=0;i<wallet.length;i++){
-   fetch(`https://api.ethermine.org/miner/:${wallet[i]}/workers`,{
-     method: 'GET'
-   })
-  .then(res => res.json())
-  .then(res => { 
-    if(res.status === "OK"){
-    list = res.data
-      .filter(item => item.currentHashrate*100/item.reportedHashrate <= 90)
-      .map(item => `
-        worker: ${item.worker}
-        lastSeen: ${item.lastSeen}
-        reportedHashrate: ${item.reportedHashrate}
-        currentHashrate: ${item.currentHashrate}
-        ${Math.floor(item.currentHashrate*100/item.reportedHashrate)} %
+	const wallet = []
+
+	for (let i = 0; i < wallet.length; i++) {
+		fetch(`https://api.ethermine.org/miner/:${wallet[i]}/workers`, {
+				method: 'GET'
+			})
+			.then(res => res.json())
+			.then(res => {
+				if (res.status === "OK") {
+					list = res.data
+			.filter(item => item.currentHashrate * 100 / item.reportedHashrate <= 80)
+			.map(item => `
+			worker: ${item.worker}
+			lastSeen: ${item.lastSeen}
+			reportedHashrate: ${item.reportedHashrate}
+			currentHashrate: ${item.currentHashrate}
+			${Math.floor(item.currentHashrate*100/item.reportedHashrate)} %
         `)
-      .join('\n') 
-    data += list
-
-
-  }}) 
-  .catch((err) => console.log(err, "failed"))
-  }
+			.join('\n')
+			if(list !== "") data += list 
+			if(nTime() - oldTime  >= 600000) err = true
+			nTime()			
+			}
+			})
+			.catch((err) => console.log(err, "failed"))
+	}
 }
 datas()
 
 client.on("message", (msg) => {
-  const { author } = msg
+	const {
+		author
+	} = msg
 
-  // Check if user is a bot
-  if (author.bot) return
-  if (msg.content === "!moris") {
-    msg.reply("Start");
-   
-    setInterval(() => {
-    if(data.length > 0){
-      datas();
-      msg.reply(data)
-      data = "";
-    }},3000)
-}})
+	// Check if user is a bot
+	if (author.bot) return
+	if (msg.content === "!moris") {
+		msg.reply("Start");
+		setInterval(() => {
+			datas();
+			if (data.length > 0 && err === true) {	
+				msg.reply(data)
+				oldTime = nTime()
+				data = "";
+       			err = false
+			}
+		}, 6000)
+	}
+})
 
 client.login(config.token)
