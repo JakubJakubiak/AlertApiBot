07d93bdfec0e6879f01d0ace2046b12eb8422e53
Jakub Jakubiak <jebacyuu@gmail.com>
2022-01-16 19:32:47 +0100

    big

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv").config({ path: __dirname + "/./../../.env" })
 
 module.exports = {
-  TOKEN: process.env.token,
+  TOKEN: process.env.Token,
   Wallet: process.env.WALLET
 }
